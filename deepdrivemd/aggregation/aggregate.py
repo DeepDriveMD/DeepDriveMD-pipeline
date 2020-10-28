bfa10714d8d7bca236e3f338e79f95f362326d43
braceal <braceal@umich.edu>
2020-10-28 16:57:51 -0400

    data aggregation script

---
+++ deepdrivemd/aggregation/aggregate.py
@@ -0,0 +1,85 @@
+import h5py
+import numpy as np
+from deepdrivemd.api import DeepDriveMD_API
+from deepdrivemd.config import get_config, AggregationConfig
+
+
+def concatenate_all(cfg: AggregationConfig):
+
+    fields = []
+    if cfg.rmsd:
+        fields.append("rmsd")
+    if cfg.fnc:
+        fields.append("fnc")
+    if cfg.contact_map:
+        fields.append("contact_map")
+    if cfg.point_cloud:
+        fields.append("point_cloud")
+
+    # Get list of input h5 files
+    api = DeepDriveMD_API(cfg.experiment_directory)
+    files = api.get_last_n_hdf5(n=cfg.last_n_h5_files)
+
+    if cfg.verbose:
+        print(f"Collected {len(files)} h5 files.")
+
+    # Open output file
+    fout = h5py.File(cfg.out_path, "w", libver="latest")
+
+    # Initialize data buffers
+    data = {x: [] for x in fields}
+
+    for in_file in files:
+
+        if cfg.verbose:
+            print("Reading", in_file)
+
+        with h5py.File(in_file, "r") as fin:
+            for field in fields:
+                data[field].append(fin[field][...])
+
+    # Concatenate data
+    for field in data:
+        data[field] = np.concatenate(data[field])
+
+    # Centor of mass (CMS) subtraction
+    if "point_cloud" in data:
+        if cfg.verbose:
+            print("Subtract center of mass (CMS) from point cloud")
+        cms = np.mean(
+            data["point_cloud"][:, 0:3, :].astype(np.float128), axis=2, keepdims=True
+        ).astype(np.float32)
+        data["point_cloud"][:, 0:3, :] -= cms
+
+    # Create new dsets from concatenated dataset
+    for field, concat_dset in data.items():
+        if field == "traj_file":
+            utf8_type = h5py.string_dtype("utf-8")
+            fout.create_dataset("traj_file", data=concat_dset, dtype=utf8_type)
+            continue
+
+        shape = concat_dset.shape
+        chunkshape = (1,) + shape[1:]
+        # Create dataset
+        if concat_dset.dtype != np.object:
+            if np.any(np.isnan(concat_dset)):
+                raise ValueError("NaN detected in concat_dset.")
+            dset = fout.create_dataset(
+                field, shape, chunks=chunkshape, dtype=concat_dset.dtype
+            )
+        else:
+            dset = fout.create_dataset(
+                field, shape, chunks=chunkshape, dtype=h5py.vlen_dtype(np.int16)
+            )
+        # write data
+        dset[...] = concat_dset[...]
+
+    # Clean up
+    fout.flush()
+    fout.close()
+
+
+if __name__ == "__main__":
+    cfg = get_config()
+    cfg = AggregationConfig.from_yaml(**cfg)
+    concatenate_all(cfg)
