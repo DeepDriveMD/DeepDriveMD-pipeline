cfe5b8f486c81925be4b4e5c1e139b3ec5dd15c6
braceal <braceal@umich.edu>
2020-12-15 16:47:26 -0500

    added writepdb method to data api

--- deepdrivemd/data/api.py
+++ deepdrivemd/data/api.py
@@ -1,6 +1,9 @@
 import itertools
 from pathlib import Path
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Union
+import MDAnalysis
+
+PathLike = Union[str, Path]
 
 
 def glob_file_from_dirs(dirs: List[str], pattern: str) -> List[str]:
@@ -9,7 +12,7 @@ def glob_file_from_dirs(dirs: List[str], pattern: str) -> List[str]:
 
 
 class DeepDriveMD_API:
-    def __init__(self, experiment_directory: Path):
+    def __init__(self, experiment_directory: PathLike):
         self.experiment_dir = Path(experiment_directory)
 
     def get_last_n_md_runs(self, n: Optional[int] = None) -> Dict[str, List[str]]:
@@ -28,6 +31,44 @@ class DeepDriveMD_API:
 
         return {
             "h5_files": glob_file_from_dirs(run_dirs, "*h5"),
-            "dcd_files": glob_file_from_dirs(run_dirs, "*dcd"),
+            "traj_files": glob_file_from_dirs(run_dirs, "*dcd"),
             "pdb_files": glob_file_from_dirs(run_dirs, "*pdb"),
         }
+
+    @staticmethod
+    def write_pdb(
+        output_pdb_file: PathLike,
+        input_pdb_file: PathLike,
+        traj_file: PathLike,
+        frame: int,
+    ):
+        r"""Write a PDB file.
+
+        Writes `output_pdb_file` to disk containing coordindates of
+        a single `frame` from a given input PDB `input_pdb_file` and
+        trajectory file `traj_file`.
+
+        Parameters
+        ----------
+        output_pdb_file : Union[str, Path]
+            The path of the output PDB file to be written to.
+        input_pdb_file : Union[str, Path]
+            The path of the input PDB file used to open `traj_file`
+            in MDAnalysis.Universe().
+        traj_file : Union[str, Path]
+            The path of the trajectory file to be read from.
+        frame : int
+            The frame index into `traj_file` used to write `output_pdb_file`.
+
+        Examples
+        --------
+        >>> output_pdb_file = "/path/to/output.pdb"
+        >>> input_pdb_file = "/path/to/input.pdb"
+        >>> traj_file = "/path/to/traj.dcd"
+        >>> frame = 10
+        >>> write_pdb(output_pdb_file, input_pdb_file, traj_file, frame)
+        """
+        u = MDAnalysis.Universe(str(input_pdb_file), str(traj_file))
+        u.trajectory[frame]
+        PDB = MDAnalysis.Writer(str(output_pdb_file))
+        PDB.write(u.atoms)
