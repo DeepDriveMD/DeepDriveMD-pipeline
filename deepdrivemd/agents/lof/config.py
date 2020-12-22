d07dfdceeaec0a7971ae0a6d96c24e47de8f644c
braceal <braceal@umich.edu>
2020-12-22 18:39:42 -0500

    cleaned up lof code. factored out the machine learning model

--- deepdrivemd/agents/lof/config.py
+++ deepdrivemd/agents/lof/config.py
@@ -1,25 +1,29 @@
+from enum import Enum
 from deepdrivemd.config import AgentTaskConfig
 
 
 class LOFConfig(AgentTaskConfig):
     """LOF outlier detection algorithm configuration."""
 
+    class ModelType(str, Enum):
+        aae = "AAE3d"
+
     # Number of outliers to detect (should be number of MD jobs + 1, incase errors)
     num_outliers: int = 100
     # Number of frames in each trajectory/HDF5 file
     n_traj_frames: int = 200
-    # Number of points in point cloud AAE
-    num_points: int = 304
-    # Inference batch size for encoder forward pass
-    inference_batch_size: int = 128
-    # Inference forward pass device
-    device: str = "cuda:0"
     # Select the n most recent HDF5 files for outlier search
     last_n_h5_files: int = 10
     # Select k random HDF5 files from previous DeepDriveMD iterations for outlier search
     k_random_old_h5_files: int = 0
     # Number of workers to use for LOF
     sklearn_num_jobs: int = -1
+    # Which machine learning model to use
+    model_type: ModelType = ModelType.aae
+    # Inference batch size for encoder forward pass
+    inference_batch_size: int = 128
+    # Inference forward pass device
+    device: str = "cuda:0"
 
 
 if __name__ == "__main__":
