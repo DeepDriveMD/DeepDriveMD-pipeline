683acc08d66da792fac1ae86b418a2b03d45b527
braceal <braceal@umich.edu>
2021-01-09 13:42:13 -0500

    allow option to select a portion of the agent data runs

--- deepdrivemd/data/analysis.py
+++ deepdrivemd/data/analysis.py
@@ -9,10 +9,12 @@ class DeepDriveMD_Analysis:
     def __init__(self, experiment_directory: PathLike):
         self.api = DeepDriveMD_API(experiment_directory)
 
-    def get_agent_data(self) -> List[List[Dict[str, Any]]]:
+    def get_agent_data(self, iterations: int = -1) -> List[List[Dict[str, Any]]]:
+        if iterations == -1:
+            iterations = self.api.get_total_iterations()
         agent_json_data = [
             self.api.agent_stage.read_task_json(stage_idx)
-            for stage_idx in range(self.api.get_total_iterations())
+            for stage_idx in range(iterations)
         ]
         assert None not in agent_json_data
         return agent_json_data
