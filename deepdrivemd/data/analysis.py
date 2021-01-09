bd112b3a9699a0ce67bdade6fb332cbaee2882cf
braceal <braceal@umich.edu>
2021-01-09 14:45:21 -0500

    function to get agent h5 file

--- deepdrivemd/data/analysis.py
+++ deepdrivemd/data/analysis.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import numpy as np
 from typing import Union, List, Dict, Any
 from deepdrivemd.data.api import DeepDriveMD_API
+from deepdrivemd.data.utils import parse_h5
 
 PathLike = Union[str, Path]
 
@@ -9,7 +11,7 @@ class DeepDriveMD_Analysis:
     def __init__(self, experiment_directory: PathLike):
         self.api = DeepDriveMD_API(experiment_directory)
 
-    def get_agent_data(self, iterations: int = -1) -> List[List[Dict[str, Any]]]:
+    def get_agent_json(self, iterations: int = -1) -> List[List[Dict[str, Any]]]:
         if iterations == -1:
             iterations = self.api.get_total_iterations()
         agent_json_data = [
@@ -18,3 +20,16 @@ class DeepDriveMD_Analysis:
         ]
         assert None not in agent_json_data
         return agent_json_data
+
+    def get_agent_h5(
+        self, iterations: int = -1, fields: List[str] = []
+    ) -> List[Dict[str, np.ndarray]]:
+        if iterations == -1:
+            iterations = self.api.get_total_iterations()
+        h5_data = [
+            parse_h5(
+                next(self.api.agent_stage.stage_dir(stage_idx).glob("**/*.h5")), fields
+            )
+            for stage_idx in range(iterations)
+        ]
+        return h5_data
