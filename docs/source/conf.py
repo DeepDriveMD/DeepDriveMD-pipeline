7ea9af2e24870aa3602c79145bd1c177d2b74205
braceal <braceal@umich.edu>
2021-05-12 18:04:54 -0400

    conf.py updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,7 @@ copyright = "2020-{}, ".format(now.year) + author
 
 # The full version, including alpha/beta/rc tags
 release = deepdrivemd.__version__
+version = deepdrivemd.__version__
 
 
 # -- General configuration ---------------------------------------------------
@@ -63,4 +64,4 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
