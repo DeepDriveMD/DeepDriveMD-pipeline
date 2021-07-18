7c6d79ca05304ca24370281c2693c023935a9764
Igor Yakushin <yakushin@lassen709.coral.llnl.gov>
2021-07-18 09:27:07 -0700

    reusing SimulationContext from non-streaming version

--- deepdrivemd/sim/openmm_stream/openmm_reporter.py
+++ deepdrivemd/sim/openmm_stream/openmm_reporter.py
@@ -1,12 +1,9 @@
 import simtk.openmm.app as app
-# import simtk.openmm as omm
 import simtk.unit as u 
 import os
 from MDAnalysis.analysis import distances, rms
 import MDAnalysis
 import numpy as np 
-# import h5py 
-# import sys
 from deepdrivemd.utils import t1Dto2D, t2Dto1D, hash2intarray
 
 import adios2
