e2508d75997b0a7b48fc150f3aa27fc0b2aa74da
Igor Yakushin <yakushin@lassen708.coral.llnl.gov>
2021-08-21 09:25:31 -0700

    final_shape to config

--- deepdrivemd/agents/stream/config.py
+++ deepdrivemd/agents/stream/config.py
@@ -68,6 +68,7 @@ class OutlierDetectionConfig(AgentTaskConfig):
     use_outliers: bool = True
     use_random_outliers: bool = False
     compute_rmsd: bool = True
+    final_shape: List[int] = [28, 28, 1]
 
 
 if __name__ == "__main__":
