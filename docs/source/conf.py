94db5993a531d835454c3dec5170b2a4c5279104
braceal <braceal@umich.edu>
2022-05-16 20:41:49 -0500

    docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,17 @@ templates_path = ["_templates"]
 exclude_patterns = []
 
 # List of imports to mock when building the documentation.
-autodoc_mock_imports = ["adios2", "tensorflow", "simtk.openmm", "cupy", "cuml", "numba"]
+autodoc_mock_imports = [
+    "adios2",
+    "tensorflow",
+    "simtk.openmm",
+    "cupy",
+    "cuml",
+    "numba",
+    "torch",
+    "torchsummary",
+    "mdlearn",
+]
 
 html_context = {
     "conf_py_path": "/docs/source/",  # Path in the checkout to the docs root
