e0ddc7d6677ca7aa769c74e0a5e34a31c521b86f
Huub van Dam <hvandam@bnl.gov>
2023-07-22 22:47:23 -0400

    Turn HDF5 file locking off.
    File locking seems to cause problems on Crusher on the compute nodes
    for no apparent reason.

--- bin/run.sh
+++ bin/run.sh
@@ -6,6 +6,21 @@ which radical-stack
 hostname
 
 radical-stack
+
+# HDF5 often fails to lock files on compute nodes
+# so best turn locking off to prevent failures
+# in the reporter.
+#
+# The error message in question is:
+# "Unable to create file (unable to lock file, errno = 524, error message = 'Unknown error 524')"
+#
+# More information on this error:
+# https://github.com/nanoporetech/medaka/issues/240
+#
+# See for the reporter h5py code: 
+# - MD-tools/mdtools/openmm/reporter.py
+# - MD-tools/mdtools/nwchem/reporter.py
+export HDF5_USE_FILE_LOCKING="FALSE"
  
 #python  -m deepdrivemd.deepdrivemd_stream -c bba/$1/config.yaml
 python  -m deepdrivemd.deepdrivemd -c bba/$1/config.yaml
